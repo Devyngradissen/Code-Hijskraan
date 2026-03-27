309ce9c0adb589665f762ed0672040f0b7a4e351
Devyngradissen <devyngradissen@gmail.com>
2026-03-27 13:00:52 +0100

    V1.1
    Dit zijn de templates voor de Camera, Grijper en Sensor

---
+++ Grijper/Template.ino
@@ -0,0 +1,11 @@
+//Deze template is om de code te pushen in github zodat de map er in staat
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
